b8fc058b3af3d523c47805b97e0886d7fcbba237
Christian <christianaaron@gmail.com>
2024-04-05 08:01:52 +0200

    created the file WorldofGames for Flask AIP and modified memory game to call the API

--- MemoryGame.py
+++ MemoryGame.py
@@ -4,20 +4,25 @@ from Utils import input_with_validation_integer
 import time
 import random
 import sys
+import requests
 
-
+#The function sends the difficulty t the backend to generate a random sequence
 def generate_sequence(difficulty):
-    randomlist = []
-    for i in range(0, difficulty):
-        randomlist.append(random.randint(1, 101))
-    return randomlist
-
-
+    json_post = {"difficulty": f"{difficulty}"}
+    response = requests.post("http://127.0.0.1:30000/Generate_Sequence", json=json_post)
+    randomlist = response.json()
+    return randomlist["sequence"]
+#Get the numbers from the users and send them to the backend server to generate the user list
 def get_list_from_user(difficulty):
-    userlist = []
     for i in range(0, difficulty):
-        userlist.append(input_with_validation_integer("Enter a number: "))
-    return userlist
+        number = input_with_validation_integer("Enter a number: ")
+        json_post = {"number": f"{number}"}
+        requests.post("http://127.0.0.1:30000/User_list", json=json_post)
+    response = requests.get("http://127.0.0.1:30000/User_list")
+    userlist = response.json()
+    print(type(userlist["userlist"]), userlist["userlist"])
+    return userlist["userlist"]
+
 
 
 def is_list_equal(lista, listb):
@@ -28,6 +33,7 @@ def is_list_equal(lista, listb):
 
 def play(difficulty):
     print(f"Your Difficulty is {difficulty}")
+    #call the generate sequence function
     randomlist = generate_sequence(difficulty)
     print("Focuse on the number in",)
     #Counter to tell the user to start concentrate before the number appears
@@ -42,21 +48,22 @@ def play(difficulty):
 
     time.sleep(0.7)
 
-    ## Hide the numbers after 0.7 seconds
+    #Hide the numbers after 0.7 seconds
 
     sys.stdout.write('\r')
     sys.stdout.flush()
 
     print("Try to remember")
-
+    #call the user list function
     userlist = get_list_from_user(difficulty)
 
     if is_list_equal(randomlist,userlist):
-        print("You have Won the Memory Game")
         # add the score to scores.txt
         Score.add_score(difficulty)
+        return {"Result": True, "Message": "You have Won the Memory Game"}
+
     else:
-        print("You have lost the Memory Game")
+        return {"Result": False, "Message" : "You have lost the Memory Game"}
 
 
 
--- WorldofGamesAPI.py
+++ WorldofGamesAPI.py
@@ -1,10 +1,51 @@
 import os
-from flask import Flask,abort,request
+from flask import Flask, abort, request, jsonify
 import Utils
 import MemoryGame
+import random
+from copy import deepcopy
 
 app = Flask(__name__)
 
+# Global Variables FOR THE API
+
+USERLIST = []
+
+
+# API Endpoint to Generate a Random Sequence of Numbers for the Memory Game
+@app.post("/Generate_Sequence")
+def generate_sequence():
+    if request.is_json:
+        # randmlist is ephemeral
+        randomlist = []
+        data = request.get_json()
+        for i in range(0, int(data["difficulty"])):
+            randomlist.append(random.randint(1, 101))
+        return jsonify({"sequence": randomlist}), 200
+    else:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+
+# API Endpoint to post the numbers of the USERLIST, get USERLIST and DELETE the USERLIST
+@app.post("/User_list")
+def post_user_list():
+    if request.is_json:
+        number = request.get_json()
+        USERLIST.append(int(number["number"]))
+        return jsonify({"message": "User number added successfully"}), 201
+    else:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+
+@app.get("/User_list")
+def get_user_list():
+    list_to_send = deepcopy(USERLIST)
+    # Clear USERLIST for the next game
+    USERLIST.clear()
+    return jsonify({"userlist": list_to_send}), 200
+
+
+@app.delete
 @app.route("/Score")
 def score_server():
     try:
@@ -19,7 +60,7 @@ def score_server():
                         </body>
                 </html>"""
     except:
-            return f"""<html>
+        return f"""<html>
                             <head>
                                     <title>Scores Game</title>
                             </head>
@@ -29,18 +70,7 @@ def score_server():
                     </html>"""
 
 
-@app.post("/MemoryGame")
-
-def play_memory_game():
-    if request.is_json:
-        data = request.get_json()
-        MemoryGame.play(data[difficulty])
-
-
-
-
-
 if __name__ == '__main__':
-    #host = os.getenv('FLASK_HOST', '0.0.0.0')
+    # host = os.getenv('FLASK_HOST', '0.0.0.0')
     port = os.getenv('FLASK_PORT', '30000')
-    app.run(port=int(port))
+    app.run(port=int(port), debug=True)
